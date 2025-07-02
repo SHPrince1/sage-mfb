4bdbb39ddabe2541e1d7967b3c199933cd59129d
shprince1 <princehsylvanus@gmail.com>
2025-07-02 19:22:12 +0100

    Navbar styled

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Logo from "../assets/lensqr-logo.svg";
 import style from "../styles/navbar.module.css";
-
+// imported Images
+import ProfileImage from "../assets/profile-image.png";
+// React Icons
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { MdArrowDropDown } from "react-icons/md";
+import { FaSearch } from "react-icons/fa";
 const NavBar = () => {
   return (
     <div>
@@ -12,11 +17,30 @@ const NavBar = () => {
         <div className={style.logoInput}>
           <div>
             <a href="/">
-              <img src={Logo} alt="" />
+              <img src={Logo} alt="" width={100} />
             </a>
           </div>
-          <div>
+          <div className={style.inputSearchDiv}>
             <input placeholder="Searchfor anything" />
+
+            
+              <button  className={style.SearchDiv}>
+                <FaSearch color="#fff" />
+              </button>
+          
+          </div>
+        </div>
+
+        <div className={style.profileImage}>
+          <div>
+            <IoIosNotificationsOutline size={25} />
+          </div>
+
+          <div className={style.proImageName}>
+            <img src={ProfileImage} alt="" />
+            {/* <p>Image</p> */}
+            <p>Adedeji</p>
+            <MdArrowDropDown />
           </div>
         </div>
       </nav>
